9c61139b6b3de7261a9234190291f812df4d9c27
muzammil <muzammil.azam88@gmail.com>
2021-07-15 19:59:40 +0500

    two way binding example (class component)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-// test commit
-function App() {
-  return (
-    <div className="App">
-     <h1>Hello world</h1>
-    </div>
-  );
+// import logo from './logo.svg';
+// import './App.css';
+// // test commit
+// function App() {
+//   return (
+//     <div className="App">
+//      <h1>Hello world</h1>
+//     </div>
+//   );
+// }
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { value: '0' };
+  }
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+  };
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <p>{this.state.value}</p>
+        <button onClick={()=>{this.setState({ value: "12300" })}}>Buy</button>
+
+
+      </div>
+    );
+  }
 }
+
 
 export default App;
