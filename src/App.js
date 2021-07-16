eb1ba77c0d27be883ce2e8cca11981ac3b9e711a
muzammil <muzammil.azam88@gmail.com>
2021-07-16 11:32:24 +0500

    update Test app 1.0(With name)

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 // import logo from './logo.svg';
 // import './App.css';
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 const App = () => {
-  const [value, setValue] = useState(""); // put default value here () 
-  
+  const [value, setValue] = useState(""); // put default value here ()
+
   useEffect(() => {
     console.log("do a task when the variable value changes");
   }, [value]);
 
   const handleChange = (e) => setValue(e.target.value);
-  
+
   return (
     <div>
-      <input
-        type="text"
-        value={value}
-        onChange={handleChange}
-      />
-      <p>{value?value:"empty"}</p>
-      <button onClick={()=> setValue("12300")}>Buy</button>
+      <input type="text" value={value} onChange={handleChange} />
+      <p>{value ? value : "empty"}</p>
+      <button onClick={() => setValue("Muzammil Azam")}>Buy</button>
     </div>
   );
-}
+};
 
 export default App;
