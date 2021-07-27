b15f59fd1f52f7557649be2027a5446a4702d6e8
muzammil <muzammil.azam88@gmail.com>
2021-07-27 17:34:15 +0500

    Delete Elements from array by using click event.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@
 
 import React, { useState } from "react";
 import "./App.css";
-import person from "./Person/Person.js";
 import Person from "./Person/Person.js";
 
 // NOW WE WRITE OUR CODE AS FUNCTIONAL COMPONENT
@@ -19,135 +18,42 @@ const App = (props) => {
     textAlign: "center",
   };
 
-  const persons = [
+  const [persons, setPersons] = useState([
     { name: "Muzammil Azam", age: 24 },
     { name: "Ghassan", age: 24 },
     { name: "Muhammad Haris", age: 23 },
-  ];
+  ]);
   const [showPersons, setShowPersons] = useState(true);
 
-  // const [state, setState] = useState({
-  //   persons: [
-  //     { name: "Muzammil Azam", age: 24 },
-  //     { name: "Ghassan", age: 24 },
-  //     { name: "Muhammad Haris", age: 23 },
-  //   ],
-  //   otherState: "some other values",
-  //   showPersons: true,
-  // });
-
-  //const [otherState, setOtherState] = useState("some other value");
-
-  // class App extends Component {
-  //   // var something =someValue;
-
-  //   state = {
-  //     persons: [
-  //       { name: "Muzammil Azam", age: 24 },
-  //       { name: "Muteeb", age: 24 },
-  //       { name: "Muhammad Haris", age: 23 },
-  //     ],
-  //     otherState: 'some other values'
-  //   };
-  //   switchNameHandler = () => {
-
-  //       //console.log('was clicked! ');
-  //       // DON'T DO THIS: this.state.persons[0].name ='Muzammil';
-  //       this.setState({
-  //         persons: [
-  //         { name: "Muzammil", age: 24 },
-  //         { name: "Muteeb", age: 24 },
-  //         { name: "Muhammad Haris", age: 23 },
-  //       ]
-  //       } )
-
-  //   }
-
-  // THERE IS NO RENDER METHOD IN FUNCTIONAL COMPONENT ONLY RETURN
-
-  //   render() {
-  //     return (
-  //       <div className="App">
-  //         <h1>Hi, this is my first app in react</h1>
-  //         <button onClick={this.switchNameHandler}>Switch Button</button>
-
-  //         {/* <Person name="Muzammil Azam" age="23" />
-  //         <Person name="Muneeb Zubair " age="22" />
-  //         <Person name="Abraham Peter " age="24" />
-  //         <Person name="Muzammil Azam" age="23">
-  //           {" "}
-  //           My hobbies: Watch Seasons
-  //         </Person> */}
-  //         <person
-  //           name={this.state.persons[0].name}
-  //           age={this.state.persons[0].age}
-  //         />
-  //         <person
-  //           name={this.state.persons[1].name}
-  //           age={this.state.persons[1].age}
-  //         />
-  //         <person
-  //           name={this.state.persons[2].name}
-  //           age={personState.persons[2].age}
-  //         />
-  //       </div>
-  //     );
-  //   }
-  // }
-
-  const switchNameHandler = () => {
-    console.log("was clicked! ");
-    // DON'T DO THIS: this.state.persons[0].name ='Muzammil';
-    console.log("state.persons[0].age", persons[0].age);
-    // setState({
-    //   persons: [
-    //     { name: "M  ", age: state.persons[0].age + 1 },
-
-    //     { name: "Muneeb zubair", age: state.persons[1].age + 1 },
-    //     { name: "Muhammad Haris", age: 23 },
-    //   ],
-    //   otherState: state.otherState,
-    //   showPersons: state.showPersons,
-    // });
+  //
+  const deletePersonHandler = async (personIndex) => {
+    const personsCopy = persons.filter((p, i) => personIndex !== i);
+    setPersons(personsCopy);
   };
-
   const togglePersonHandler = () => {
     const doesShow = showPersons;
     setShowPersons(!doesShow);
-    // setState({
-    //   persons: { ...state.persons },
-    //   showPersons: !doesShow,
-    //   otherState: "ji",
-    // });
   };
 
-  let Persons = null;
+  let Persons = "null";
   if (showPersons) {
     Persons = (
       <div>
-        {persons.map((persons) => {
-          return <Persons name={persons.name} age={persons.age} />;
+        {persons.map((person, index) => {
+          return (
+            <Person
+              onClick={(event) => {
+                deletePersonHandler(index);
+                console.log(event);
+              }}
+              name={person.name}
+              age={person.age}
+            />
+          );
         })}
-
-        {/* <person name={persons[0].name} age={persons[0].age} />
-        <Person
-          name={persons[1].name}
-          age={persons[1].age}
-          click={switchNameHandler}
-        >
-          my Hobbies: Racing
-        </Person>
-
-        <Person name={persons[2].name} age={persons[2].age}>
-          my Hobbies: Racing
-        </Person> */}
       </div>
     );
   }
-
-  // Alternative Method
-  // const togglePersonHandler = () =>
-  //   setState({ showPersons: !state.showPersons });
 
   return (
     <div className="App">
@@ -157,43 +63,137 @@ const App = (props) => {
         Switch Button.
       </button>
 
-      {/* <Person name="Muzammil Azam" age="23" />
-      <Person name="Muneeb Zubair " age="22" />
-      <Person name="Abraham Peter " age="24" />
-      <Person name="Muzammil Azam" age="23">
-        {" "}
-        My hobbies: Watch Seasons
-      </Person>
-      <person
-        name={personState.persons[0].name}
-        age={personState.persons[0].age}
-      />
-      <person
-        name={personState.persons[1].name}
-        age={personState.persons[1].age}
-      />
-      <person
-        name={personState.persons[2].name}
-        age={personState.persons[2].age}
-      /> */}
-      {Persons ? (
-        <div>
-          <person name={persons[0].name} age={persons[0].age} />
-          <Person
-            name={persons[1].name}
-            age={persons[1].age}
-            click={switchNameHandler}
-          >
-            my Hobbies: Racing
-          </Person>
-
-          <Person name={persons[2].name} age={persons[2].age}>
-            my Hobbies: Racing
-          </Person>
-        </div>
-      ) : null}
+      {Persons}
     </div>
   );
 };
 
 export default App;
+
+// const [state, setState] = useState({
+//   persons: [
+//     { name: "Muzammil Azam", age: 24 },
+//     { name: "Ghassan", age: 24 },
+//     { name: "Muhammad Haris", age: 23 },
+//   ],
+//   otherState: "some other values",
+//   showPersons: true,
+// });
+
+//const [otherState, setOtherState] = useState("some other value");
+
+// class App extends Component {
+//   // var something =someValue;
+
+//   state = {
+//     persons: [
+//       { name: "Muzammil Azam", age: 24 },
+//       { name: "Muteeb", age: 24 },
+//       { name: "Muhammad Haris", age: 23 },
+//     ],
+//     otherState: 'some other values'
+//   };
+// const switchNameHandler = () => {
+
+//     //console.log('was clicked! ');
+//     // DON'T DO THIS: this.state.persons[0].name ='Muzammil';
+//     setShowPersons({
+//       persons: [
+//       { name: "Muzammil", age: 24 },
+//       { name: "Muteeb", age: 24 },
+//       { name: "Muhammad Haris", age: 23 },
+//     ]
+//     } )
+
+// }
+
+// THERE IS NO RENDER METHOD IN FUNCTIONAL COMPONENT ONLY RETURN
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Hi, this is my first app in react</h1>
+//         <button onClick={this.switchNameHandler}>Switch Button</button>
+
+//         {/* <Person name="Muzammil Azam" age="23" />
+//         <Person name="Muneeb Zubair " age="22" />
+//         <Person name="Abraham Peter " age="24" />
+//         <Person name="Muzammil Azam" age="23">
+//           {" "}
+//           My hobbies: Watch Seasons
+//         </Person> */}
+//         <person
+//           name={this.state.persons[0].name}
+//           age={this.state.persons[0].age}
+//         />
+//         <person
+//           name={this.state.persons[1].name}
+//           age={this.state.persons[1].age}
+//         />
+//         <person
+//           name={this.state.persons[2].name}
+//           age={personState.persons[2].age}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+// const switchNameHandler = () => {
+//   console.log("was clicked! ");
+//   // DON'T DO THIS: this.state.persons[0].name ='Muzammil';
+//   console.log("state.persons[0].age", persons[0].age);
+// setState({
+//   persons: [
+//     { name: "M  ", age: state.persons[0].age + 1 },
+
+//     { name: "Muneeb zubair", age: state.persons[1].age + 1 },
+//     { name: "Muhammad Haris", age: 23 },
+//   ],
+//   otherState: state.otherState,
+//   showPersons: state.showPersons,
+// });
+// console.log("[1,2,3,4,5].splice(2,3)", [1, 2, 3, 4, 5].splice(2, 3));
+
+// setState({
+//   persons: { ...state.persons },
+//   showPersons: !doesShow,
+//   otherState: "ji",
+// });
+
+/* <Person name="Muzammil Azam" age="23" />
+    <Person name="Muneeb Zubair " age="22" />
+    <Person name="Abraham Peter " age="24" />
+    <Person name="Muzammil Azam" age="23">
+    {" "}
+    My hobbies: Watch Seasons
+    </Person>
+    <person
+    name={personState.persons[0].name}
+    age={personState.persons[0].age}
+    />
+    <person
+    name={personState.persons[1].name}
+    age={personState.persons[1].age}
+    />
+    <person
+    name={personState.persons[2].name}
+    age={personState.persons[2].age}
+    /> */
+
+/* <person name={persons[0].name} age={persons[0].age} />
+  <Person
+  name={persons[1].name}
+  age={persons[1].age}
+  click={switchNameHandler}
+  >
+  my Hobbies: Racing
+  </Person>
+  
+  <Person name={persons[2].name} age={persons[2].age}>
+  my Hobbies: Racing
+  </Person> */
+
+// Alternative Method
+// const togglePersonHandler = () =>
+//   setState({ showPersons: !state.showPersons });
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -10,7 +10,7 @@ const person = (props) => {
         I'm {props.name} and my age is {Math.floor(Math.random() * 25)} years
         old{" "}
       </p> */}
-      <p onClick={props.click}>
+      <p onClick={props.onClick}>
         I'm {props.name} and I am {props.age} years old{" "}
       </p>
       <p>{props.children}</p>
